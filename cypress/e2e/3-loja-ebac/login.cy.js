ef03cf5e97f951878d46e192cee6f1588ff6583e
Vagner Lima <Vagner_lima97@hotmail.com>
2025-07-30 14:59:55 -0300

    primeiro testev automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe('funcionalidade: login', () => {
+
+it('deve fazer login com sucesso', () => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.get('#username').type('vagnerteste.teste@hotmail.com')
+    cy.get('#password').type('teste@123')
+    cy.get('.woocommerce-form > .button').click()
+
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, vagnerteste.teste (não é vagnerteste.teste? Sair)')
+})
+
+})
